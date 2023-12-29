8c0f13acc3fe4b817fa8a11df6f0784759fd4484
Franklin Borges <franklin-jorge@hotmail.com>
2023-12-29 14:14:46 -0500

    configured prisma-test-enviroment for creation of a temporaly database for e2e tests

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,35 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('No DATABASE_URL env variable set')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
-
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+    process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma migrate deploy')
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$queryRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE;`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
