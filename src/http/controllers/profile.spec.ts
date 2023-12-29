02503a7a491dd0efcd164caf126dcdf7659643c0
Franklin Borges <franklin-jorge@hotmail.com>
2023-12-29 15:13:13 -0500

    creation of Profile (e2e) test

--- src/http/controllers/profile.spec.ts
+++ src/http/controllers/profile.spec.ts
@@ -1,3 +1,40 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { describe, it, afterAll, beforeAll, expect } from 'vitest'
+import { a } from 'vitest/dist/suite-SvxfaIxW'
 
-test('ok', () => {})
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Franklin Jorge',
+      email: 'franklin-jorge@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'franklin-jorge@example.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/profile')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(profileResponse.status).toBe(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'franklin-jorge@example.com',
+      }),
+    )
+  })
+})
