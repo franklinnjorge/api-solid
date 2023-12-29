6807d1654bfee0b0334d83a0ae2bbaf372f8bd88
Franklin Borges <franklin-jorge@hotmail.com>
2023-12-29 14:47:07 -0500

    creation of Register (e2e) test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { describe, it, afterAll, beforeAll, expect } from 'vitest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to regist a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Franklin Jorge',
+      email: 'franklin-jorge@example.com',
+      password: '123456',
+    })
+    expect(response.status).toBe(201)
+  })
+})
